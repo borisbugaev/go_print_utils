4e0194aa19aa07a15511d4269c9fa77e56251364
borisbugaev <zippityzop@protonmail.com>
2025-07-09 17:35:40 -0400

    add functions

--- console_print_utils.go
+++ console_print_utils.go
@@ -1,12 +1,15 @@
 package printutils
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 const C_UP string = "\x1b[1A"
 const L_CLR string = "\x1b[2K"
+const ABC_LEN int = 'Z' - 'A'
 
 func clear_lines(count int) {
 	for range count {
@@ -14,7 +17,41 @@ func clear_lines(count int) {
 	}
 }
 
-func print_lines(line string) int {
-	fmt.Printf("%s", line)
-	return strings.Count(line, "\n")
+func print_lines(lines string) int {
+	fmt.Printf("%s", lines)
+	return strings.Count(lines, "\n")
+}
+
+func mc_letter(index int) string {
+	my_letter := ""
+	if index > ABC_LEN {
+		letter_mod := index % ABC_LEN
+		letter_intdiv := index / ABC_LEN
+		my_letter = fmt.Sprintf("%s%s", mc_letter(letter_intdiv), mc_letter(letter_mod))
+	} else {
+		my_letter = fmt.Sprintf("%c", 'A'+index)
+	}
+	return my_letter
+}
+
+func line_select_mc(fields []string) string {
+	index := 0
+	my_fields := ""
+	fields_map := map[string]string{}
+	for _, field := range fields {
+		letter := mc_letter(index)
+		my_fields = fmt.Sprintf("%s%s> %s\n", my_fields, letter, field)
+		fields_map[letter] = field
+		index++
+	}
+	my_fields = fmt.Sprintf("%s>> ", my_fields)
+	line_count := print_lines(my_fields)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	selected_field, ok := fields_map[strings.ToUpper(scanner.Text())]
+	if !ok {
+		selected_field = "\a"
+	}
+	clear_lines(line_count)
+	return selected_field
 }
